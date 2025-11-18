fe8f487309ecb2526db8b7068034ba5d1dab3210
Ibrahim Cesar <email@ibrahimcesar.com>
2025-11-18 08:58:50 -0300

    Create basic_line.rs

---
+++ examples/basic_line.rs
@@ -0,0 +1,19 @@
+//! Basic line plot example
+
+fn main() {
+    println!("🦖 velociplot - Basic line plot example");
+    println!("🚧 Coming soon...");
+    
+    // Future API:
+    // use velociplot::prelude::*;
+    // 
+    // let x = vec![0.0, 1.0, 2.0, 3.0, 4.0];
+    // let y = vec![0.0, 1.0, 4.0, 9.0, 16.0];
+    // 
+    // plot()
+    //     .line(&x, &y)
+    //     .xlabel("X axis")
+    //     .ylabel("Y axis")
+    //     .title("Basic Line Plot")
+    //     .save("basic_line.png")?;
+}
